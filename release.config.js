bdf74a62ea494b8a382f036b7f20598e5f51045b
siaikin <abc1310054026@outlook.com>
2024-03-28 00:39:35 +0800

    feat: 自动发布时更新 package.json 版本号并构建脚本文件
    (cherry picked from commit fb22a6d951c21b66f7f01250013ec48059df8e79)

--- release.config.js
+++ release.config.js
@@ -4,12 +4,29 @@ export default {
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     [
+      "@semantic-release/npm",
+      {
+        npmPublish: false
+      }
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "npm run build"
+      }
+    ],
+    [
       "@semantic-release/changelog",
       {
         changelogFile: "CHANGELOG.md"
       }
     ],
-    "@semantic-release/git",
+    [
+      "@semantic-release/git",
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json', 'dist/*']
+      }
+    ],
   ],
   repositoryUrl: "https://github.com/siaikin/baidu-index-export.git"
 };
